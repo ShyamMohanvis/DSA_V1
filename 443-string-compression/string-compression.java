7552b8b71484b40599fdf535b7dce8bd50b89f3e
Shyam Mohan <Shyammohanvis@gmail.com>
2025-10-14 10:38:56 +0530

    Time: 11 ms (5.24%) | Memory: 44.9 MB (5.09%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int compress(char[] chars) {
+        String s = "" + chars[0];
+        int count = 1;
+        for (int i = 1; i < chars.length; i++) {
+            char curr = chars[i];
+            char prev = chars[i - 1];
+            if (curr == prev) {
+                count++;
+            } else {
+                if (count > 1) s += count;
+                s += curr;
+                count = 1;
+            }
+        }
+        if (count > 1) s += count;
+        for (int i = 0; i < s.length(); i++) chars[i] = s.charAt(i);
+        return s.length();
+    }
+}
