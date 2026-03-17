5e75f7e8c76c49839f5d24d8c0f47871f1661f7a
Shyam Mohan <Shyammohanvis@gmail.com>
2026-03-17 07:19:29 +0530

    Time: 24 ms (26.49%) | Memory: 76.5 MB (98.39%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,17 +1,13 @@
-import java.util.HashMap;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (freq.containsKey(nums[i])) {
-                
+        Arrays.sort(nums);
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
-            } else {
-                freq.put(nums[i], 1); // first time seeing nums[i]
             }
         }
-        return false; 
+        
+        return false;        
     }
 }
