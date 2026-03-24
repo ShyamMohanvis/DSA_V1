972e81c8c321d345a4eae3e5a9086d355fae3382
Shyam Mohan <Shyammohanvis@gmail.com>
2026-03-24 23:37:32 +0530

    Time: 12 ms (39.04%) | Memory: 47.7 MB (61.60%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,18 +1,28 @@
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        int[] count = new int[n + 1]; 
-        int dup = -1, miss = -1;
+        int actual_sum = n * (n + 1) / 2;
+        int array_sum = 0;
+        int unique_sum = 0;
+        Set<Integer> s = new HashSet<>();
 
-        for (int num : nums) {
-            count[num]++;
+        for (int a : nums) {
+            array_sum += a;
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (count[i] == 2) dup = i;
-            if (count[i] == 0) miss = i;
+        for (int a : nums) {
+            s.add(a);
         }
 
-        return new int[]{dup, miss};
+        for (int a : s) {
+            unique_sum += a;
+        }
+
+        int missing = actual_sum - unique_sum;
+        int duplicate = array_sum - unique_sum;
+
+        return new int[]{duplicate, missing};
     }
 }
+
